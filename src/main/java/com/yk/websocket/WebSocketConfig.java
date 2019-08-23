fb73bec10829ffb1079334880739268fccfad444
yangkai <740591134@qq.com>
2019-08-23 10:50:59 +0800

    增加注释

--- src/main/java/com/yk/websocket/WebSocketConfig.java
+++ src/main/java/com/yk/websocket/WebSocketConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
+//外置web服务器运行，屏蔽@Configuration
 @Configuration
 public class WebSocketConfig {
 	@Bean
